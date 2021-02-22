889ee201e98a27e3a05c740683c14faa83a0d911
libertizalis <libertizalis@gmail.com>
2021-02-22 20:16:18 -0300

    commit

---
+++ README.md
@@ -0,0 +1,11 @@
+web browser cwkg
+
+to compile: <br>
+mkdir _build && cd _build <br>
+cmake .. <br>
+make <br>
+
+to run: <br>
+./cwkg <br>
+
+<img src="https://github.com/libertizalis/cwkg/blob/master/doc/screenshot.png">
---
+++ _build/CMakeFiles/cwkg.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../BrowserMarshal.c"
+  "../BrowserMarshal.h"
+  "CMakeFiles/cwkg.dir/BrowserCellRendererVariant.c.o"
+  "CMakeFiles/cwkg.dir/BrowserDownloadsBar.c.o"
+  "CMakeFiles/cwkg.dir/BrowserMarshal.c.o"
+  "CMakeFiles/cwkg.dir/BrowserSearchBox.c.o"
+  "CMakeFiles/cwkg.dir/BrowserSettingsDialog.c.o"
+  "CMakeFiles/cwkg.dir/BrowserTab.c.o"
+  "CMakeFiles/cwkg.dir/BrowserWindow.c.o"
+  "CMakeFiles/cwkg.dir/main.c.o"
+  "cwkg"
+  "cwkg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cwkg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
